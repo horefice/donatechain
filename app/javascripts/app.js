a3915da69b29f965bd281efce1d41e0a02a446c5
Henrique Rodrigues Orefice <h.orefice@gmail.com>
2016-11-18 11:44:05 -0200

    initial commit

---
+++ app/javascripts/app.js
@@ -0,0 +1,74 @@
+var accounts;
+var account;
+
+function setStatus(message) {
+  var status = document.getElementById("status");
+  status.innerHTML = message;
+};
+
+function refreshBalance() {
+  var meta = MetaCoin.deployed();
+
+  meta.getBalance.call(account, {from: account}).then(function(value) {
+    var balance_element = document.getElementById("balance");
+    balance_element.innerHTML = value.valueOf();
+  }).catch(function(e) {
+    console.log(e);
+    setStatus("Error getting balance; see log.");
+  });
+};
+
+function sendCoin() {
+  var meta = MetaCoin.deployed();
+
+  var amount = parseInt(document.getElementById("amount").value);
+  var receiver = document.getElementById("receiver").value;
+
+  setStatus("Initiating transaction... please wait");
+
+  meta.sendCoin(receiver, amount, {from: account}).then(function() {
+    setStatus("Transaction complete!");
+    refreshBalance();
+  }).catch(function(e) {
+    console.log(e);
+    setStatus("Error sending coin; see log.");
+  });
+};
+
+window.onload = function() {
+  web3.eth.getAccounts(function(err, accs) {
+    if (err != null) {
+      alert("There was an error fetching your accounts.");
+      return;
+    }
+
+    if (accs.length == 0) {
+      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+      return;
+    }
+
+    accounts = accs;
+    account = accounts[0];
+
+    refreshBalance();
+  });
+
+  document.getElementById("send").addEventListener("click", function(){sendCoin()});
+}
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
